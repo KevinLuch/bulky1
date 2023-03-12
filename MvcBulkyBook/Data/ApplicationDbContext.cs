a0472d9041b80391dfc209e8fa36c6bfac4b8170
Kevin <kevinluch@gmail.com>
2023-03-11 18:57:49 -0600

    first commit

---
+++ MvcBulkyBook/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcBulkyBook.Models;
+
+namespace MvcBulkyBook.Data
+{
+    public class ApplicationDbContext :DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
